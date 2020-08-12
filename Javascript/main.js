165ccc2ca304d4bcd8fe0e04b38ff0185de4853a
daniaramburu <aramburudf@gmail.com>
2020-08-12 19:09:25 -0300

    sumo práctica

---
+++ Javascript/main.js
@@ -0,0 +1,4 @@
+let nombredelusuarix =prompt("Cómo es tu nombre?")
+let apellidodelusuarix =prompt("Cómo es tu apellido?")
+alert("Hola" + " "+ nombredelusuarix + " " + apellidodelusuarix + ", Bienvenidx a ADA")
+
