133328a69bd6258cf7d8cb63f21224b5458ea818
daniaramburu <aramburudf@gmail.com>
2020-08-12 22:10:45 -0300

    agregue ejercicios javascritp

---
+++ Javascript/Ejercicios/main.js
@@ -0,0 +1,55 @@
+// Ejercitación de JS
+
+//1
+const sumar = (num1, num2) => {
+    return (num1 + num2)
+
+}
+
+sumar(2,3)
+sumar(1.2,3.4)
+sumar(3,-5)
+
+console.log (sumar(2,3))
+console.log (sumar(1.2,3.4))
+console.log (sumar (3,-5))
+
+//2
+
+const restar = (num1, num2) => {
+    return (num1 - num2)
+}
+
+restar(3,2)
+restar(10,5.5)
+restar(3,5)
+
+console.log (restar(3,2))
+console.log (restar(10,5.5))
+console.log (restar(3,5))
+
+//3
+
+const multiplicar = (num1,num2) => {
+    return (num1 * num2)
+}
+
+multiplicar (2,3)
+multiplicar (4,0.5)
+
+console.log (multiplicar(2,3))
+console.log (multiplicar(4,0.5))
+
+//4
+
+const dividir = (num1,num2) => {
+    return (num1/num2)
+}
+
+dividir (2,3)
+dividir (1.2,3.4)
+dividir (3,-5)
+
+console.log (dividir(2,3))
+console.log (dividir(1.2,3.4))
+console.log(dividir(3,-5))
